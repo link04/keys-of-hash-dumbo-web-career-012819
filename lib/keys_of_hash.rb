44e7b94bfd39d650eadea829d170209fbf9de9ec
Maximo Bautista <max.bautista97@gmail.com>
2019-01-11 04:19:17 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,9 +7,19 @@ class Hash
     hash = self
     
     arguments.each do |value|
-      value == "Panama"? array << "red-footed tortoise" 
-      
+      value == "Panama" ? array << "red-footed tortoise" 
+      value == "Madagascar" ? array.push("aye-aye","tomato frog") 
+      value == "Panama" ? array.push("sugar glider", "kangaroo", "koala") 
     end
+    
+    hash.each do |key, value|
+        arguments.each do |item|
+          if value == item
+            array << key
+          end
+        end
+      end
+    
     
     if arguments.include? "Panama"
       array << "red-footed tortoise"
