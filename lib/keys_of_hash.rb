1d903c11db090f20aa5afd9749be59bb91b08890
Maximo Bautista <max.bautista97@gmail.com>
2019-01-11 03:59:12 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,18 +4,8 @@ class Hash
   
   def keys_of(*arguments)
     animal = []
-    hash = keys_of.self
-    if arguments.include? "Panama"
-      animal << "red-footed tortoise"
-    elsif arguments.include?   "Madagascar"
-      animal = ["aye-aye","tomato frog"] 
-    elsif arguments.include?  "Australia"
-      animal = ["sugar glider", "kangaroo", "koala"] 
-    else 
-        animal <<  self
-        ok = hash
-    end
-    animal
+    hash = self
+
     binding.pry
   end
   
