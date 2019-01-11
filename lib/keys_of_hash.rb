1df88418b3e86b04fb4fb85ecfc842cde8da0d6d
Maximo Bautista <max.bautista97@gmail.com>
2019-01-11 04:27:34 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,31 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  
+  def keys_of(*arguments)
+    array = []
+    hash = self
+    
+    if hash.is_a?(Array)
+      arguments.each do |value|
+        case value
+        when "Panama" 
+          array << "red-footed tortoise"
+        when "Madagascar" 
+          array.push("aye-aye","tomato  frog")
+        when "Panama"
+          array.push("sugar glider", "kangaroo", "koala")
+        end
+      end
+    else
+      hash.each do |key, value|
+        arguments.each do |item|
+          if value == item
+            array << key
+          end
+        end
+      end
+    end
+    array
   end
 end
